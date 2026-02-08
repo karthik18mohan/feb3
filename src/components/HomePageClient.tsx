4e9ba3a1efcb7e45954adf3a7d1f70d0241a2c80
karthik18mohan <karthik18mohan@gmail.com>
2026-02-08 18:10:15 +0530

    Add carousel slide for manual to automatic

--- src/components/HomePageClient.tsx
+++ src/components/HomePageClient.tsx
@@ -34,6 +34,10 @@ const landingSlides = [
   {
     src: "/images/landing-page/3.jpg",
     caption: "Rooted in Tradition. Ready for Tomorrow"
+  },
+  {
+    src: "/images/4.jpg",
+    caption: "Manual to Automatic"
   }
 ];
 
